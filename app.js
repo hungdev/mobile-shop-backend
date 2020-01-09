0fe45b72c5396bc88c803e3a6a0581825615614c
Cee <cee@Cees-MacBook-Pro.local>
2020-01-09 20:08:55 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config()
 
 const indexRouter = require('./routes/index');
@@ -37,6 +38,7 @@ mongoose.connect('mongodb://localhost:27017/mobileShopMongoDB', options).then(
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
